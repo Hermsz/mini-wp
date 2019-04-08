6117af49cff69cda4e9556f0206ce8d556b64465
hermsz <email@hermi.ltd>
2019-04-08 20:52:06 +0700

    first upload mini wp

---
+++ client/main.js
@@ -0,0 +1,9 @@
+$('#add-article-page-reveal-button').click(function() {
+  $("#blog-posts-page").hide()
+  $("#add-article-page").show()
+})
+
+$('#back-article-page-reveal-button').click(function() {
+  $("#blog-posts-page").hide()
+  $("#add-article-page").show()
+})
